d7cfcea54017121d06285ee1aae35f7c66cd8d26
parthibCsaha <sahaparthib19@gmail.com>
2026-02-20 10:08:35 +0600

    added repos

---
+++ backend/src/main/java/com/backend/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.backend.repository;
+
+import com.backend.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
